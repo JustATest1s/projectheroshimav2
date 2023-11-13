57f96b706b70f3ab9bb3463ac0205b491ff6bf20
JustATest1s <120183708+JustATest1s@users.noreply.github.com>
2023-11-13 13:07:06 +0000

    updating index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var password = "12345";
 
 function passcheck() {
 
-if(document.petElementById('projectheroshima').value != password)  {
+if(document.getElementById('projectheroshima').value != password)  {
 alert('Wrong Password, Try Again.');   
 return false;
 }
 
-if(document.petElementById('projectheroshima').value == password)  {
+if(document.getElementById('projectheroshima').value == password)  {
 alert('Correct Password. Click OK to enter webpage');   
 }
 }
